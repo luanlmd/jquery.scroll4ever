a507b7061b6e93f56126ae19ba4c376794f48640
Luan Almeida <luanlmd@gmail.com>
2012-04-11 10:40:46 -0300

    Totally rebuilt, a different idea, a better one

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 
 // Begin random crap generator
-
 $words = array('lorem','ipsum','dolor','ftw','sit','amet');
 $news = array();
 function phrase()
@@ -18,47 +17,53 @@ for ($x = 0; $x < 1000; $x++)
 {
 	$news[] = array($x, phrase());
 }
-
 // End random crap generator
 
 // Pagination
 $page = (@$_GET['page'])? $_GET['page'] : 1;
 $offset = $page * 20;
 $start = $offset - 20;
+
 ?>
-<!DOCTYPE html> 
-<html> 
-	<head> 
-		<title>Scroll4Ever - jQuery Infinite Scroll plugin</title> 
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Scroll4Ever - jQuery Infinite Scroll plugin</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<style> 
+		<style>
+			* { margin:0; padding:0; margin:0; }
+			body { padding:20px; }
+			h1, p { margin:10px 0; }
 			li { background: #ccc; display:block; margin:1px; padding:15px; }
+			.trigger { padding:5px 10px; background:red; color:black; display:block; margin:20px 0; }
 		</style>
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 		<script src="jquery.scroll4ever.js"></script>
 		<script>
 			$(document).ready(function()
 			{
 				$('.scroll4ever').scroll4ever({
-					url:document.URL + '?page=', // data source url, could be another page. page number will always be added in the end
-					selector:'.scroll4ever li', // this is the selection made in the source url data
-					distance:100, // distance to the end of window to trigger the new 'page'
+					trigger:'.scroll4ever .trigger',
+					container: '.scroll4ever',
+					selector:'.scroll4ever > *', // this is the selection made in the source url data
+					distance:100, // distance to the end of window to trigger page load, if avoided, it will not auto trigger
 					debug: true, // if you want some messages in console...
 					start: function(){}, // callback called when a new page load begins, good for loading messages
 					complete: function(){}, // callback called when a new page load ends
-					container: false, // where the new content must be loaded, main selection is the default one
-					skip: false // number of itens to skip, default is to use page numbering and server side skip/offset calculation
 				});
 			});
 		</script>
-	</head> 
+	</head>
 	<body>
 		<h1>Awesome news</h1>
 		<p>Scroll the window to load more and more.</p>
-		<ul class="scroll4ever">
-			<?php for($x = $start; $x < $offset; $x++) { ?>
-				<li><?= $news[$x][0] ?> - <?= $news[$x][1] ?></li>
-			<?php } ?>
-		</ul>
+		<div class="scroll4ever">
+			<ul>
+				<?php for($x = $start; $x < $offset; $x++) { ?>
+					<li><?= $news[$x][0] ?> - <?= $news[$x][1] ?></li>
+				<?php } ?>
+			</ul>
+			<a class="trigger" href="?page=<?= ++$page ?>" rel="next">Load more stuff like that!</a>
+		</div>
 	</body>
 </html>
--- jquery.scroll4ever.js
+++ jquery.scroll4ever.js
@@ -1,62 +1,58 @@
 jQuery.fn.scroll4ever = function(options)
 {
+	var settings = $.extend({
+        trigger : false,
+        container: $(this),
+        selector: false,
+        distance : 0,
+        start : function(){},
+        complete : function(){},
+        debug : false
+    }, options);
+
 	var loading = false;
-	var ended = false;
-	
-	var options = options || []
-	var debug = options['debug'] || false;
-	var url = options['url'] || false;
-	var selector = options['selector'] || false;
-	var skip = options['skip'] || false;
-	var page = skip/skip || 2;
-	var distance = options['distance'] || 250;
-	var start = options['start'] || function(){};
-	var complete = options['complete'] || function(){};
-	var container = options['container'] || $(this);
-		
-		
-	console.log($(this));
+
 	log = function(obj)
 	{
-		 if (debug) { console.log(obj); }
+		 if (settings.debug) { console.log(obj); }
 	}
-	
-	log(url);
+
 	log('initialized');
 
-	load = function()
-	{
-		start();
-		loading = true;
-		
-		var next = (skip)? skip*page : page;
-		log('loading: ' + url + next);
-		$.ajax(url + next,{ success:function(data)
-		{
-			
-			if (selector) { data = $(data).find(selector); }
-			//console.log(data);
-			if (!data.length) { ended = true; log('no more data, ended'); }
-			else
-			{
-				container.append(data);
-				page++;
-				$(window).trigger('scroll');
-			}
-			loading = false;
-			complete();
-			
-		}, dataType:'xml'});
+	log(settings);
 
-	}
-	
-	$(window).scroll(function(){
-		if ($(document).scrollTop() >= ($(document).height() - $(window).height() - distance))
-		{
-			if (!ended && !loading)
-			{
-				load();
-			}
-		}
+	log($(this));
+
+	$(settings.trigger).live('click', function(){
+
+		log('running start');
+		settings.start();
+		loading = true;
+
+		url = $(this).attr('href') + ' ' + settings.selector;
+		div = $('<div />').load(url, function() {
+			$(settings.trigger).remove();
+			$(settings.container).append(div.find('> *'));
+
+			loading = false;
+
+			log('running complete');
+			settings.complete();
+		});
+
+		return false;
 	});
+
+	if (settings.distance)
+	{
+		$(window).scroll(function(){
+			if ($(document).scrollTop() >= ($(document).height() - $(window).height() - settings.distance))
+			{
+				if (!loading)
+				{
+					$(settings.trigger).trigger('click');
+				}
+			}
+		});
+	}
 };
